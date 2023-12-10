a85343e6ff5336a0ed447bcbcf09847359301e2f
myshuker <myshuker@gmail.com>
2023-12-10 01:47:07 +0100

    end the game

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import GameCard from "./gameCard/GameCard";
 
+const winProbability = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
-  const [cell, setCell] = useState(["", "", "", "", "", "", "", "", ""])
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("1");
+  const [winner, setWinner] = useState("");
+
+  useEffect(() => {
+    winProbability.forEach((win) => {
+      const playerFirstWin = win.every((cell) => cells[cell] === "x");
+      const playerSecWin = win.every((cell) => cells[cell] === "o");
+      if (playerFirstWin) {
+        setWinner(" The Winner is Player 1");
+      } else if (playerSecWin) {
+        setWinner("The Winner is Player 2");
+      }
+    });
+  }, [cells, winner]);
+
+  useEffect(() => {}, [cells, winner]);
+  if (cells.every((cell) => cell !== "") && !winner) {
+    setWinner("Draw!");
+  }
+
+  const resultStyle = {
+    color: "blue",
+  };
 
   return (
     <div className="App">
       <div className="gameBox">
-        {cell.map((item, index) => (
-          <GameCard key={index} />
+        {cells.map((cell, index) => (
+          <GameCard
+            key={index}
+            id={index}
+            player={player}
+            setPlayer={setPlayer}
+            cells={cells}
+            setCells={setCells}
+            winner={winner}
+          />
         ))}
       </div>
+      {!winner && (
+        <h1>
+          {`Turn Player `} {player}
+        </h1>
+      )}
+      <h2 style={resultStyle}> {winner} </h2>
     </div>
   );
 }
--- src/gameCard/GameCard.js
+++ src/gameCard/GameCard.js
@@ -1,12 +1,35 @@
-import './gameCard.css'
+import { useState } from "react";
+import "./gameCard.css";
 
-function GameCard(props) {
-  const clickHandler = () => {
-    console.log('clicked');
-  }
+function GameCard({ id, player, setPlayer, cells, setCells, winner }) {
+  const [sign, setSign] = useState("");
+  let newCellArr = [...cells];
+
+  const clickHandler = (e) => {
+    if (!winner) {
+      if (sign === "") {
+        if (player === "1") {
+          setSign("X");
+          setPlayer("2");
+          newCellArr[id] = "x";
+          setCells(newCellArr);
+        } else if (player === "2") {
+          setSign("O");
+          setPlayer("1");
+          newCellArr[id] = "o";
+          setCells(newCellArr);
+        }
+      } else {
+        alert("Press another Place");
+      }
+    }
+  };
+
   return (
-    <div className='gameCard' onClick={clickHandler} >card</div>
-  )
+    <div className="gameCard" onClick={clickHandler}>
+      <div className={sign}> {sign}</div>
+    </div>
+  );
 }
 
-export default GameCard
+export default GameCard;
